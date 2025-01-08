0588796ba14ee71d12894ba5682f29d1ab4cc662
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-01-07 16:58:57 -0800

    add dt joystick curve

--- TeamCode/src/main/java/mechanisms/Drivetrain.java
+++ TeamCode/src/main/java/mechanisms/Drivetrain.java
@@ -37,9 +37,9 @@ public class Drivetrain {
 
 
     public void Loop(Gamepad gp1, Gamepad gp2){
-        double y = gp1.left_stick_y; // Remember, Y stick value is reversed
-        double x = -gp1.left_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = -gp1.right_stick_x * 0.6;
+        double y = Math.pow(gp1.left_stick_y, 3); // Remember, Y stick value is reversed
+        double x = Math.pow(gp1.left_stick_x, 3) * -1.1; // Counteract imperfect strafing
+        double rx = Math.pow(gp1.right_stick_x, 3) * -0.6;
 
 //        if (gp1.dpad_up) {
 //            x = 0; y = -MICROSPEED;
